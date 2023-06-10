504a9c2369f620904931b8b73d774adbf9e25a8a
Robin van der Linde <robin.vanderlinde@gmail.com>
2023-06-10 22:21:47 +0200

    Fix hreflang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,12 @@
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <svelte:head>
 	{#each $locales as locale}
-		<link rel="alternate" hreflang={locale} href={`/${locale}/`} />
+		<link rel="alternate" hreflang={locale} href={`${$page.url.origin}/${locale}/`} />
 	{/each}
 </svelte:head>
 <div>
